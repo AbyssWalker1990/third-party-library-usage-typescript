131175a680d7edc2f6f58c1b0ebab0e0040b02cd
AbyssWalker1990 <bordiian.v@gmail.com>
2023-03-02 18:29:16 +0200

    configured tsconfig and installed dependencies properly

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
-// Code goes he
-console.log('lol')
-const a = 10
+import dotenv from 'dotenv'
+dotenv.config()
+
+const form = document.querySelector('form')
+const addressInput = document.getElementById('address') as HTMLInputElement
+
+function searchAddressHandler (event: Event): void {
+  event.preventDefault()
+  if (addressInput != null) {
+    const enteredAddress = addressInput.value
+  }
+
+  // send to google api
+}
+
+if (form != null) {
+  form.addEventListener('submit')
+}
