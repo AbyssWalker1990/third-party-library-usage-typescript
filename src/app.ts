b4afe710bebd63c9a7eafded92a96dcc67403c92
Vova <bordiian.v@gmail.com>
2023-03-03 11:12:51 +0200

    fetched result with coordinates

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { API_KEY } from '../config'
 
+interface Results {
+  results: Array<{ geometry: { location: { lat: number, lng: number } } }>
+  status: 'OK' | 'ZERO_RESULTS'
+}
+
 const form = document.querySelector('form')
 const addressInput = document.getElementById('address') as HTMLInputElement
 
@@ -12,11 +17,17 @@ function searchAddressHandler (event: Event): void {
   }
 
   // send to google api
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY as string}`)
+  axios
+    .get<Results>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY as string}`)
     .then(response => {
-      console.log(response)
+      if (response.data.status !== 'OK') {
+        throw new Error('Couldnt fetch location')
+      }
+      const coordinates = response.data.results[0].geometry.location
     })
     .catch(err => {
+      alert(err.message)
       console.log(err)
     })
 }
